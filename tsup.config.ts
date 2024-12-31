746f606bcb96434d6a2734322e7fb6c7310ee2a2
Andrew Tran <andrewtranva@gmail.com>
2024-12-30 19:17:15 -0500

    Added entrypoint for 'tsup'

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from "tsup";
  */
 export default defineConfig({
   format: ["cjs", "esm"],
-  entry: ["src/"],
+  entry: ["./src/index.ts"],
   dts: true,
   shims: true,
   skipNodeModulesBundle: true,
